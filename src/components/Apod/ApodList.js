cf063dc19dc4b0c03c7b602c548e2f21219bcf14
Brian Hague <alphaseinor@gmail.com>
2019-09-26 21:48:16 -0500

    added reactstrap

--- src/components/Apod/ApodList.js
+++ src/components/Apod/ApodList.js
@@ -1,14 +1,17 @@
 import React, {useEffect, useState} from "react";
+import {ApodFrame} from "./ApodFrame";
 
 import axios from 'axios';
 
+import {Container, Row} from "reactstrap"
+
 export function ApodList(){
-  const [photos, setPhotos] =useState([])
+  const [photo, setPhoto] =useState([])
   
-  useEffect(() => {
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2018-09-25`)
+  useEffect(() => { 
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=EwTD8nntFpQflSE62RAmu8EfYamxWLU95x1Gau7D`)//&date=2018-09-25
     .then(res => {
-      console.log(res)
+      setPhoto(res.data)
     })
     .catch(err => {
       console.log(err)
@@ -17,9 +20,20 @@ export function ApodList(){
       console.log(`girlfriend, how did you let him treat you so bad, wo, wo, wo, wo, wo`)
     })
   }, [])
+  console.log(photo)
 
   return (
-    <div></div>
+    <Container>
+      <Row>
+        <ApodFrame
+          date= {photo.date}
+          title= {photo.title}
+          description= {photo.explanation}
+          src= {photo.url}
+          type= {photo.media_type}
+        />
+      </Row>
+    </Container>
   )
    
 }
